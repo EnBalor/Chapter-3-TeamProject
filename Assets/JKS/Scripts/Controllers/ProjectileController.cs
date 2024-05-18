45abc010dab79363d6b55580d7beadcc1952e4b0
MothorMoth <mothormoth@gmail.com>
2024-05-19 00:29:03 +0900

    Feature: 발사체 충돌 시 폭발 애니메이션 추가

--- Assets/JKS/Scripts/Controllers/ProjectileController.cs
+++ Assets/JKS/Scripts/Controllers/ProjectileController.cs
@@ -1,19 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
 {
     private SpriteRenderer _spriteRenderer;
     private Rigidbody2D _rigidbody;
+    private Animator _animator;
 
     private CharacterStat _currentStat;
     private Vector2 _direction;
     private float _currentDuration;
     private bool _isReady;
+    private bool _isDestroyed;
 
     private void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -27,23 +31,28 @@ public class ProjectileController : MonoBehaviour
 
         if (_currentDuration > _currentStat.duration)
         {
-            DestroyProjectile();
+            DestroyProjectile(false);
         }
 
-        _rigidbody.velocity = _direction * _currentStat.attackSpeed;
+        if (_isDestroyed)
+        {
+            _rigidbody.velocity = Vector2.zero;
+        }
+        else
+        {
+            _rigidbody.velocity = _direction * _currentStat.attackSpeed;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // currentStat.target에 포함되는 레이어인지 확인
         if (IsLayerMatched(_currentStat.target.value, collision.gameObject.layer))
         {
             // TODO: HealthSystem
-            DestroyProjectile();
+            DestroyProjectile(true);
         }
     }
 
-    // 레이어가 일치하는지 확인하는 메소드
     private bool IsLayerMatched(int layerMask, int objectLayer)
     {
         return layerMask == (layerMask | (1 << objectLayer));
@@ -61,6 +70,7 @@ public class ProjectileController : MonoBehaviour
         transform.right = this._direction;
 
         _isReady = true;
+        _isDestroyed = false;
     }
 
     private void UpdateProjectileSprite()
@@ -68,7 +78,22 @@ public class ProjectileController : MonoBehaviour
         transform.localScale = Vector3.one * _currentStat.size;
     }
 
-    private void DestroyProjectile()
+    private void DestroyProjectile(bool createFx)
+    {
+        _isDestroyed = true;
+
+        if (createFx)
+        {
+            _animator.SetTrigger("Explosion");
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    // 에디터 Animator에서 이벤트 할당
+    private void DelaySetActive()
     {
         gameObject.SetActive(false);
     }
